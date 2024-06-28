c7d6c3986f495a4e81458a5a28055e75ab127efd
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-06-28 16:13:42 -0300

    Eliminio la carpeta dist del repositorio

--- dist/modules/persons.js
+++
@@ -1,9 +0,0 @@
-import { User } from './users.js';
-export class Person extends User {
-    constructor(id, email, contact, type, created_at, is_active, first_name, last_name) {
-        super(id, email, contact, type, created_at, is_active);
-        this.first_name = first_name;
-        this.last_name = last_name;
-    }
-}
-//# sourceMappingURL=persons.js.map
