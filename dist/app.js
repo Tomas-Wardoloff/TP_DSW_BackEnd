55cf895af012f0b2d2d84c14da9e4dcabebb7c09
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-06-27 23:40:44 -0300

    Agrego las rutas de la api club al servidor

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { athleteRouter } from "./athletes/athlete.routes.js";
+import { clubRouter } from "./club/club.routes.js";
 const app = express();
 app.use(express.json());
 app.use('/api/athletes', athleteRouter);
+app.use('/api/clubs', clubRouter);
 app.use((_, res) => {
     return res.status(404).send({ message: "Not found" });
 });
