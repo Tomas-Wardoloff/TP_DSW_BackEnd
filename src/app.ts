0c9b1cbb45ff35d2534dbb9eece64cf6c0690e59
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-09-12 16:41:58 -0300

    Defino ruta para user

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { clubRouter } from "./club/club.routes.js"
 import { agentRouter } from "./agent/agent.routes.js"
 import { orm, syncSchema } from "./shared/db/orm.js"
 import { RequestContext } from '@mikro-orm/core'
+import { userRouter } from './users/users.routes.js'
 
 const app = express()
 app.use(express.json())
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 app.use('/api/athlete', athleteRouter)
 app.use('/api/club', clubRouter)
 app.use('/api/agent', agentRouter)
+app.use('/api/user', userRouter)
 
 app.use((_, res) => {
     return res.status(404).send({message: "Not found"})
