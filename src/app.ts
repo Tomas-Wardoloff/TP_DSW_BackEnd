56271e1239865af8eaa650970992c5487095ea21
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-09-19 12:23:01 -0300

    Defino e ignoro variable de entorno para el puero

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata'
+import dotenv from 'dotenv'
 import express from "express"
-import { athleteRouter } from "./athletes/athlete.routes.js"
+import { athleteRouter } from "./athlete/athlete.routes.js"
 import { clubRouter } from "./club/club.routes.js"
 import { agentRouter } from "./agent/agent.routes.js"
 import { orm, syncSchema } from "./shared/db/orm.js"
 import { RequestContext } from '@mikro-orm/core'
-import { userRouter } from './users/users.routes.js'
+import { userRouter } from './user/user.routes.js'
+
+dotenv.config()
 
 const app = express()
 app.use(express.json())
@@ -14,10 +17,10 @@ app.use((req, res, next) => {
     RequestContext.create(orm.em, next)
 })
 
-app.use('/api/athlete', athleteRouter)
-app.use('/api/club', clubRouter)
-app.use('/api/agent', agentRouter)
-app.use('/api/user', userRouter)
+app.use('/api/athletes', athleteRouter)
+app.use('/api/clubs', clubRouter)
+app.use('/api/agents', agentRouter)
+app.use('/api/users', userRouter)
 
 app.use((_, res) => {
     return res.status(404).send({message: "Not found"})
@@ -25,6 +28,8 @@ app.use((_, res) => {
 
 await syncSchema() // never in production
 
-app.listen(3000, () => {
-    console.log("server running on http://localhost:3000/")
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`server running on http://localhost:${port}`)
 })
