4d441ad888d286a21cfc0bd478e7c3d1d4e1cbeb
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-09-21 17:17:00 -0300

    Defino metodos add, findone, findall y remove para post

---
+++ src/post/post.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express'
+import { orm } from '../shared/db/orm.js'
+import { User } from '../user/user.entity.js'
+import { Post } from './post.entity.js'
+
+const em = orm.em
+
+async function findAll(req: Request, res: Response) {
+    try{
+        const posts = await em.find(Post, {}, {populate: ['author']})
+        res.status(200).json({message: 'found all posts',data: posts})
+    }catch (error: any){
+        res.status(500).json({message: error.message})
+    }
+}   
+
+
+async function findOne(req: Request, res: Response) {
+    try{
+        const id = Number.parseInt(req.params.id)
+        const post = await em.findOneOrFail(Post, {id}, {populate: ['author']})
+        res.status(200).json({message: 'found post', data: post})
+    }catch (error: any){
+        res.status(500).json({message: error.message})
+    }
+}
+
+
+async function add(req: Request, res: Response){
+    try{
+        const { author } = req.body
+        const relatedUser = await em.findOneOrFail('User', {id: author})
+        if (relatedUser){
+            const newPost = em.create(Post, req.body);
+            await em.flush()
+            res.status(201).json({message: 'Post created', data: newPost})
+        }
+    }catch (error: any){
+        res.status(500).json({message: error.message})
+    }
+}
+
+
+async function update(req: Request, res: Response){
+    res.status(500).json({message: "Method Not Implemented"})
+}
+
+
+async function remove(req: Request, res: Response){
+    try{
+        const id = Number.parseInt(req.params.id)
+        const postToRemove = em.getReference(Post, id)
+        await em.removeAndFlush(postToRemove)
+        res.status(200).json({message: 'Post removed'})
+    }catch (error: any){
+        res.status(500).json({message: error.message})
+    }
+}
+
+// Export the functions to be used in the routes
+export {findAll, findOne, add, update, remove}
