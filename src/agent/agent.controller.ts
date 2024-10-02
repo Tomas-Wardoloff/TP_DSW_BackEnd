768cb018e8d7e39fb434f7c434b2c0efa2c33c9a
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-10-02 12:46:04 -0300

    Cambio el nombre del parametro userId en la request para agent

--- src/agent/agent.controller.ts
+++ src/agent/agent.controller.ts
@@ -28,8 +28,8 @@ async function findOne(req: Request, res: Response) {
 
 async function add(req: Request, res:Response){
     try{
-        const { user } = req.body
-        const relatedUser = await em.findOneOrFail('User', {id: user})
+        const { userId } = req.body
+        const relatedUser = await em.findOneOrFail('User', {id: userId})
         if (relatedUser){
             const existingAgent = await em.findOne(Agent, {user: relatedUser})
             if (existingAgent){
