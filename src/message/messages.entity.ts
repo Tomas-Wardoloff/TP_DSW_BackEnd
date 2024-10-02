1eb893f6308c98d6ab6af56075b1a036b01e630d
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-10-02 13:02:16 -0300

    Modifico entidad message

--- src/message/messages.entity.ts
+++ src/message/messages.entity.ts
@@ -1,16 +1,22 @@
-import { Entity, OneToOne, Property } from "@mikro-orm/core";
+import { Entity, ManyToOne, Property } from "@mikro-orm/core";
 import { BaseEntity } from "../shared/db/baseEntity.entity.js";
 import { User } from "../user/user.entity.js";
 
 
 @Entity()
 export class Message extends BaseEntity {
-    @OneToOne(() => User)
+    @ManyToOne(() => User)
     senderUser!: User;
     
-    @OneToOne(() => User)
+    @ManyToOne(() => User)
     receiverUser!: User;
     
     @Property({length: 500})
     message!: string;
+
+    @Property({onCreate: () => new Date()})
+    sendDate!: Date;
+
+    @Property()
+    readDate!: Date;
 }
