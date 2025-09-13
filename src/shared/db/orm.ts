08c155a4d145897e79ac313e58c13fd8b85cf6de
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2025-09-13 11:16:35 -0300

    Actualizo configuracion mikro-orm

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -1,24 +1,31 @@
-import { MikroORM } from "@mikro-orm/core";
-import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
+import dotenv from 'dotenv';
+import { MikroORM } from '@mikro-orm/core';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+
+dotenv.config();
 
 export const orm = await MikroORM.init({
     entities: ['dist/**/*.entity.js'],
     entitiesTs: ['src/**/*.entity.ts'],
-    dbName: 'sportsdb',
+    dbName: process.env.DB_NAME || 'sportsdb',
     type: 'mysql',
-    clientUrl: 'mysql://dsw:dsw@localhost:3306/sportsdb',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
     highlighter: new SqlHighlighter(),
     debug: true,
-    schemaGenerator: { // never in production
+    schemaGenerator: {
+        // never in production
         disableForeignKeys: false,
         createForeignKeyConstraints: true,
         ignoreSchema: [],
     },
-})
+});
 
 export const syncSchema = async () => {
     const generator = orm.getSchemaGenerator();
-    // await generator.dropSchema();
-    // await generator.createSchema();
+    await generator.dropSchema();
+    await generator.createSchema();
     await generator.updateSchema();
-}
+};
