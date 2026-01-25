11819407230ab758670f86b12bd453b5c70cc933
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2026-01-25 12:25:57 -0600

    Add AgentRouter: implement routes for agent management operations

---
+++ src/modules/agent/agent.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import { UpdateAgentDto } from './agent.dto.js';
+import { AgentController } from './agent.controller.js';
+import { validationMiddleware } from '../../shared/middleware/validation.middleware.js';
+
+export default class AgentRouter {
+    private router = Router();
+    private agentController = new AgentController();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes() {
+        this.router.get('/', (req, res) => this.agentController.findAll(req, res));
+        this.router.get('/:id', (req, res) => this.agentController.findOne(req, res));
+        this.router.patch('/:id', validationMiddleware(UpdateAgentDto), (req, res) =>
+            this.agentController.update(req, res)
+        );
+        this.router.delete('/:id', (req, res) => this.agentController.delete(req, res));
+    }
+
+    public getRouter() {
+        return this.router;
+    }
+}
+
