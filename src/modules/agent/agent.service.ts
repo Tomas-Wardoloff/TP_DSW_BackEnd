f8248f6c986922df5c674187a104ea056102f722
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2026-01-25 12:25:51 -0600

    Add AgentService: implement CRUD operations for agent management

---
+++ src/modules/agent/agent.service.ts
@@ -0,0 +1,44 @@
+import { EntityManager } from "@mikro-orm/mysql"
+
+import { Agent } from "./agent.entity.js"
+import { orm } from "../../shared/db/orm.js"
+import { User } from "../user/user.entity.js";
+import { Club } from "../club/club.entity.js";
+import { CreateAgentDto, UpdateAgentDto } from "./agent.dto.js";
+
+export class AgentService {
+    private readonly entityManager  = orm.em as EntityManager
+
+    async findAll(): Promise<Agent[]> {
+        const filters: Partial<Agent> = {deletedAt: undefined};
+        return this.entityManager.find(Agent, filters);
+    }
+
+    async findOne(id: number): Promise<Agent | null> {
+        return this.entityManager.findOne(Agent, {id, deletedAt: null});
+    }
+
+    create(agentData: CreateAgentDto, user: User, club: Club): Agent {
+        const newAgent = this.entityManager.create(Agent, {
+            ...agentData, 
+            user, 
+            club,
+        });
+        return newAgent
+    }
+
+    async update(id: number, updateData: UpdateAgentDto): Promise<void> {
+        const agentToUpdate = await this.entityManager.findOneOrFail(Agent, {
+            id,
+            deletedAt: null,
+        });
+        this.entityManager.assign(agentToUpdate, updateData);
+        await this.entityManager.flush();
+    }
+
+    async delete(id: number): Promise<void> {
+        const agentToDelete = await this.entityManager.findOneOrFail(Agent, {id});
+        agentToDelete.deletedAt = new Date();
+        await this.entityManager.flush();
+    }
+}
