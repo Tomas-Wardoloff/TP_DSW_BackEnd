d5ba4813420466af2a35568c646765e1042d17c6
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2026-02-25 00:44:17 -1000

    Add Like entity with relationships to User and Post

---
+++ src/modules/post/like.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, Unique, Rel, ManyToOne } from "@mikro-orm/core";
+
+import { User } from "../user/user.entity";
+import { Post } from "./post.entity";
+import { BaseEntity } from "../../shared/db/baseEntity.entity.js";
+
+@Entity()
+@Unique({ properties: ['author', 'post'] })
+export class Like extends BaseEntity {
+    @ManyToOne(() => User, { eager: false })
+    author!: Rel<User>;
+
+    @ManyToOne(() => Post)
+    post!: Rel<Post>;
+}
