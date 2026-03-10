a87fbea5a2212db3e1df6df3c80e9f4e425f0c1c
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2026-03-10 15:16:31 -0300

    Fix import path to use standard 'path' module instead of 'path/win32'

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path/win32';
+import path from 'path';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { NotFoundError, RequestContext } from '@mikro-orm/core';
